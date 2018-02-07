5c4fe63a7abe8ca8fab345748858c9e67e3ff09a
hasan kiwan <email@example.com>
2018-02-07 14:27:21 -0800

    card user

--- views/site/index.php
+++ views/site/index.php
@@ -1,67 +1,67 @@
 <?php
-
+    
 /* @var $this yii\web\View */
 
 $this->title = 'I AM RIACH';
 ?>
 <div class="site-index">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card mb-3">
-                <img class="card-img-top" src="file:///C:/Users/hassanki/Desktop/11.jpg" alt="Card image cap">
+                <img class="card-img-top" src=<?php Yii::$app->request->baseUrl?>'image/11.jpg' alt="Card image cap"  class="img-circle"  width="228" height="236"  >
                     <div class="card-block">
                         <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><a href="#collapse" class="nav-toggle">Read More</a>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card mb-3">
-                <img class="card-img-top" src="C:\Users\hassanki\Desktop\11.jpg" alt="Card image cap">
+                <img class="card-img-top" src=<?php Yii::$app->request->baseUrl?>'image/11.jpg' alt="Card image cap"  class="img-circle"  width="228" height="236">
                     <div class="card-block">
                         <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><a href="#collapse" class="nav-toggle">Read More</a>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card mb-3">
-                <img class="card-img-top" src="C:\Users\hassanki\Desktop\11.jpg" alt="Card image cap">
+                <img class="card-img-top" src=<?php Yii::$app->request->baseUrl?>'image/11.jpg' alt="Card image cap"  class="img-circle"  width="228" height="236"  >
                     <div class="card-block">
                         <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><a href="#collapse" class="nav-toggle">Read More</a>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card mb-3">
-                <img class="card-img-top" src="C:\Users\hassanki\Desktop\11.jpg" alt="Card image cap">
+                <img class="card-img-top" src=<?php Yii::$app->request->baseUrl?>'image/11.jpg' alt="Card image cap"  class="img-circle"  width="228" height="236"  >
                     <div class="card-block">
                         <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><a href="#collapse" class="nav-toggle">Read More</a>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card mb-3">
-                <img class="card-img-top" src="C:\Users\hassanki\Desktop\11.jpg" alt="Card image cap">
+                <img class="card-img-top" src=<?php Yii::$app->request->baseUrl?>'image/11.jpg' alt="Card image cap"  class="img-circle"  width="228" height="236"  >
                     <div class="card-block">
                         <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><a href="#collapse" class="nav-toggle">Read More</a>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card mb-3">
-                <img class="card-img-top" src="C:\Users\hassanki\Desktop\11.jpg" alt="Card image cap">
+                <img class="card-img-top" src=<?php Yii::$app->request->baseUrl?>'image/11.jpg' alt="Card image cap"  class="img-circle"  width="228" height="236" >
                     <div class="card-block">
                         <h4 class="card-title">Card title</h4>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><a href="#collapse" class="nav-toggle">Read More</a>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
             </div>
