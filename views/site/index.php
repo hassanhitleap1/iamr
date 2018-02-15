ecfe6478be6382102ed4df804b2857e33bddda84
hasan kiwan <email@example.com>
2018-02-14 16:06:39 -0800

    string red more

--- views/site/index.php
+++ views/site/index.php
@@ -12,7 +12,7 @@ $this->title = 'I AM RIACH';
                 <img class="card-img-top" src=<?= Yii::$app->request->baseUrl.'/'.$user->image_name?> alt="Card image cap"  class="img-circle"  width="228" height="236"  >
                     <div class="card-block">
                         <h4 class="card-title"><?= $user->full_name?></h4>
-                        <p class="card-text"><?= $user->about_me?></p><a href="#collapse" class="nav-toggle">Read More</a>
+                        <p class="card-text"><?=  substr($user->about_me,0,40)?></p><a href="#collapse" class="nav-toggle">Read More</a>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
             </div>
