f74d0e74c52965579fee0e4e3494727231a74f7d
hasan kiwan <email@example.com>
2018-02-17 14:05:16 -0800

    genarate model admin

--- config/web.php
+++ config/web.php
@@ -12,6 +12,11 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
