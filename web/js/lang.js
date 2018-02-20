a94effe4b3dcdfa66ff633ce98c18ba2bac5a540
hasan <hassan.kiwan@gmail.com>
2018-02-20 15:12:39 +0200

    s

--- web/js/lang.js
+++ web/js/lang.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
-
-	 	var currentId='en';
-
         var itaImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Italien.gif";
     	var engImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Grossbritanien.gif";
 		var deuImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Deutschland.gif";
 		var fraImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Frankreich.gif";
-		var arbImgLink = "https://cdn6.aptoide.com/imgs/f/6/2/f627a82b6ece32acbf9e5bb6f4484e26_icon.png?w=256";
+		var arbImgLink = "https://www.aussiedisposals.com.au/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/J/o/Jordan.jpg";
 
 		var imgBtnSel = $('#imgBtnSel');
 		var imgBtnIta = $('#imgBtnIta');
@@ -30,7 +27,7 @@ $(document).ready(function(){
 		imgBtnEng.attr("src",engImgLink);
 		imgBtnDeu.attr("src",deuImgLink);
 		imgBtnFra.attr("src",fraImgLink);
-		imgBtnFra.attr("src",arbImgLink);
+		imgBtnArb.attr("src",arbImgLink);
 
 		imgNavSel.attr("src",itaImgLink);
 		imgNavIta.attr("src",itaImgLink);
@@ -38,70 +35,25 @@ $(document).ready(function(){
 		imgNavDeu.attr("src",deuImgLink);
 		imgNavFra.attr("src",fraImgLink);
 		imgNavArb.attr("src",arbImgLink);
-		
+
 		$( ".language" ).on( "click", function( event ) {
-
-			 currentId = $(this).attr('id');
-
-			if(currentId == "navIta") {
-				codeLang='it';
-				
-			} else if (currentId == "navEng") {
-				codeLang='en';
-
-			} else if (currentId == "navDeu") {
-				codeLang='de';
-				
-			} else if (currentId == "navFra") {
-				codeLang='fr';
-
-			}else if (currentId == "navArb") {
-				codeLang='ar';
-			}
-			
-			setLang(codeLang,currentId);
-		});// click on language 
-});
-
-function setLang(lang,currentId){
-	url='http://localhost/iamrich/web/index.php?r=base/language';
-	
-	$.ajax({
-         url: url,
-         type: 'post',
-         data: {'lang':lang},
-         success: function (data) {
-         		location.reload(); 
-         		console.log(currentId);
-         		setCookie('currentId',currentId);	
-           }
-
-     });
-}
-
-
-function setIconLang(currentId){
+			var currentId = $(this).attr('id');
 
 			if(currentId == "navIta") {
 				imgNavSel.attr("src",itaImgLink);
 				spanNavSel.text("ITA");
-				
 			} else if (currentId == "navEng") {
 				imgNavSel.attr("src",engImgLink);
 				spanNavSel.text("ENG");
-				
 			} else if (currentId == "navDeu") {
 				imgNavSel.attr("src",deuImgLink);
 				spanNavSel.text("DEU");
-				
 			} else if (currentId == "navFra") {
 				imgNavSel.attr("src",fraImgLink);
 				spanNavSel.text("FRA");
-				
 			}else if (currentId == "navArb") {
 				imgNavSel.attr("src",arbImgLink);
 				spanNavSel.text("ARB");
-				
 			}
 
 			if(currentId == "btnIta") {
@@ -116,28 +68,65 @@ function setIconLang(currentId){
 			} else if (currentId == "btnFra") {
 				imgBtnSel.attr("src",fraImgLink);
 				spanBtnSel.text("FRA");
-			}else if (currentId == "btnArb") {
+			} else if (currentId == "btnArb") {
 				imgBtnSel.attr("src",arbImgLink);
 				spanBtnSel.text("ARB");
 			}
-     
+			
+		});
+});
+
+function setCookie(key, value) 
+{
+   	var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+	document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) 
+{
+	var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+	return keyValue ? keyValue[2] : null;
+}
+
+function setVar(){
+
+	        var itaImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Italien.gif";
+    	var engImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Grossbritanien.gif";
+		var deuImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Deutschland.gif";
+		var fraImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Frankreich.gif";
+		var arbImgLink = "https://www.aussiedisposals.com.au/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/J/o/Jordan.jpg";
+
+		var imgBtnSel = $('#imgBtnSel');
+		var imgBtnIta = $('#imgBtnIta');
+		var imgBtnEng = $('#imgBtnEng');
+		var imgBtnDeu = $('#imgBtnDeu');
+		var imgBtnFra = $('#imgBtnFra');
+		var imgBtnArb = $('#imgBtnArb');
+
+		var imgNavSel = $('#imgNavSel');
+		var imgNavIta = $('#imgNavIta');
+		var imgNavEng = $('#imgNavEng');
+		var imgNavDeu = $('#imgNavDeu');
+		var imgNavFra = $('#imgNavFra');
+		var imgNavArb = $('#imgNavArb');
+
+		var spanNavSel = $('#lanNavSel');
+		var spanBtnSel = $('#lanBtnSel');
+
+		imgBtnSel.attr("src",itaImgLink);
+		imgBtnIta.attr("src",itaImgLink);
+		imgBtnEng.attr("src",engImgLink);
+		imgBtnDeu.attr("src",deuImgLink);
+		imgBtnFra.attr("src",fraImgLink);
+		imgBtnArb.attr("src",arbImgLink);
+
+		imgNavSel.attr("src",itaImgLink);
+		imgNavIta.attr("src",itaImgLink);
+		imgNavEng.attr("src",engImgLink);
+		imgNavDeu.attr("src",deuImgLink);
+		imgNavFra.attr("src",fraImgLink);
+		imgNavArb.attr("src",arbImgLink);
 }
 
 
-$(window).bind("load", function() { 
-	console.log(getCookie('currentId'));
-	setIconLang(getCookie('currentId'));
-});
-
-
-
-      function setCookie(key, value) {
-            var expires = new Date();
-            expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
-            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-        }
-
-        function getCookie(key) {
-            var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-            return keyValue ? keyValue[2] : null;
-        }
