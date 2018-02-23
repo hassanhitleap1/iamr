67b512255a27cf9797765bbbb344baba764cdbae
hasan kiwan <email@example.com>
2018-02-23 09:35:32 -0800

    import chart js

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -1,9 +1,49 @@
 <?php
 use  yii\helpers\Url; 
 use yii\widgets\LinkPager;
+use dosamigos\chartjs\ChartJs;
 /* @var $this yii\web\View */
 $this->title = 'Admin';
 ?>
 <div class="admin-default-index">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<canvas id="pie-chart" width="1000" height="200"></canvas>
+<canvas id="pie2-chart" width="1000" height="200"></canvas>
+<script>
 
+new Chart(document.getElementById("pie2-chart"), {
+    type: 'pie',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+new Chart(document.getElementById("pie-chart"), {
+    type: 'pie',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
+</script>
 </div>
