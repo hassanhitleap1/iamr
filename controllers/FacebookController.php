152c7277eb32acabb4b8a8213ec21f17dc62ac46
hasan kiwan <email@example.com>
2018-02-24 07:42:07 -0800

    facebook

--- controllers/FacebookController.php
+++ controllers/FacebookController.php
@@ -3,153 +3,39 @@
 namespace app\controllers;
 
 use Yii;
-use Facebook\Facebook; 
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\Exceptions\FacebookResponseException;
-use Facebook\Exceptions\FacebookSDKException;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
 use yii\web\Controller;
+use Facebook\Facebook;
 
 
 
-class FacebookController extends \yii\base\Controller{
+class FacebookController extends Controller
+{
 
-    public $fb;
-    public $helper;
-
-    public function __construct (){
-        // $this->fb =new Facebook\Facebook([
-        //     // 'app_id' => '148986182454458',
-        //     // 'app_secret' => '275058a24dc34f3cad39bfe29f3bf4b9   ',
-        //     // 'default_graph_version' => 'v2.10',
-        //   ]);
-
-        
-    }
-
+    private $fb;
+    private $helper;
     public function actionIndex(){
-
-        $appId         = '148986182454458'; //Facebook App ID
-        $appSecret     = '275058a24dc34f3cad39bfe29f3bf4b9'; //Facebook App Secret
+        $appId         = '394173574379206'; //Facebook App ID
+        $appSecret     = 'dc097ecfce1f15f89b39acf9300c0305'; //Facebook App Secret
         $baseUrl = 'http://localhost/'.Yii::$app->getUrlManager()->getBaseUrl();
-        $redirectURL   = "$baseUrl/index.php"; //Callback URL
+        $redirectURL   = "http://localhost/iamrich/web/index.php?r=facebook%2Fredirect"; //Callback URL
         $permissions = array('email');  //Optional permissions
-           $fb = new Facebook(array(
+        $this->fb = new Facebook(array(
             'app_id' => $appId,
             'app_secret' => $appSecret,
             'default_graph_version' => 'v2.12',
         ));
-              $helper = $fb->getRedirectLoginHelper();
-        $loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
-        //return $loginUrl;
+        $this->helper = $this->fb->getRedirectLoginHelper();
+        $loginUrl = $this->helper->getLoginUrl($redirectURL, $permissions);
+       
         return $this->redirect($loginUrl);
-        
-        exit();
-
-        // $this->helper = $this->fb->getRedirectLoginHelper();
-        // $redirectUrl="http://localhost/iamrich/web/index.php?r=facebook/redirect";
-        // $permissions = ['email']; // Optional permissions
-        // $loginUrl = $helper->getLoginUrl( $redirectUrl, $permissions);
+       
     }
-
-    // public function actionRedirect(){
-    //     try{
-    //         $accessToken =$this->helper->getAccessToken();
-    //     }catch(Facebook\Exceptions\FacebookResponseException $e){
-    //         echo "responce error exption".$e->getMessage();
-    //         exit();
-    //     }catch(Facebook\Exceptions\FacebookSDKException $e){
-    //         echo "responce error exption".$e->getMessage();
-    //         exit();
-    //     }
-
-    //     if (! isset($accessToken)) {
-    //         if ($this->helper->getError()) {
-    //           header('HTTP/1.0 401 Unauthorized');
-    //           echo "Error: " . $this->helper->getError() . "\n";
-    //           echo "Error Code: " . $this->helper->getErrorCode() . "\n";
-    //           echo "Error Reason: " . $this->helper->getErrorReason() . "\n";
-    //           echo "Error Description: " . $this->helper->getErrorDescription() . "\n";
-    //         } else {
-    //           header('HTTP/1.0 400 Bad Request');
-    //           echo 'Bad request';
-    //         }
-    //         exit;
-    //       }
- 
-    //         var_dump($accessToken->getValue());
-
-    //         // The OAuth 2.0 client handler helps us manage access tokens
-    //         $oAuth2Client = $this->fb->getOAuth2Client();
-
-    //         $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-    //         echo '<h3>Metadata</h3>';
-    //         var_dump($tokenMetadata);
-
-    //         // Validation (these will throw FacebookSDKException's when they fail)
-    //         $tokenMetadata->validateAppId('{app-id}'); // Replace {app-id} with your app id
-    //         // If you know the user ID this access token belongs to, you can validate it here
-    //         //$tokenMetadata->validateUserId('123');
-    //         $tokenMetadata->validateExpiration();
-
-    //         if (! $accessToken->isLongLived()) {
-    //         // Exchanges a short-lived access token for a long-lived one
-    //         try {
-    //             $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-    //         } catch (Facebook\Exceptions\FacebookSDKException $e) {
-    //             echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
-    //             exit;
-    //         }
-
-    //         echo '<h3>Long-lived</h3>';
-    //         var_dump($accessToken->getValue());
-    //         }
-    //        // $rresponse = $this->fb->get(endpoint:"me?fields=id,name,email,first_name,last_name");
-    //         //$_SESSION['fb_access_token'] = (string) $accessToken;
-
-
-    // }
     
-      public  function actionSignupFacebook(){
 
-        $fb = new Facebook([
-        'app_id' => '2008791992709911', // Replace {app-id} with your app id
-        'app_secret' => '9f216402f311b0d6e79c280b3191f7d7',
-        'default_graph_version' => 'v2.7',
-        ]);
-        $baseUrl = 'http://localhost/'.Yii::$app->getUrlManager()->getBaseUrl();
-        $redirectURL   = 'http://localhost/testsio/web/index.php?r=site/redirect' ;//Callback URL
-        $permissions = array('email');  //Optional permissions
-        $helper = $fb->getRedirectLoginHelper();
-        $loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
-   
-        return $this->redirect($loginUrl);
-
-    }
-
-    public function actionRedirect(){
-        $fb = new Facebook([
-            'app_id' => '2008791992709911', // Replace {app-id} with your app id
-            'app_secret' => '9f216402f311b0d6e79c280b3191f7d7',
-            'default_graph_version' => 'v2.12',
-            ]);
-
-
-            $baseUrl = 'http://localhost/'.Yii::$app->getUrlManager()->getBaseUrl();
-            $redirectURL   = 'http://localhost/testsio/web/index.php?r=site/redirect' ;//Callback URL
-            $permissions = array('email');  //Optional permissions
-            $helper = $fb->getRedirectLoginHelper();
-            $loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
-
-          var_dump($helper);
-          exit;
+        public function actionRedirect(){
         try{
             $accessToken =$this->helper->getAccessToken();
-        }
-        catch(Facebook\Exceptions\FacebookResponseException $e){
+        }catch(Facebook\Exceptions\FacebookResponseException $e){
             echo "responce error exption".$e->getMessage();
             exit();
         }catch(Facebook\Exceptions\FacebookSDKException $e){
@@ -157,8 +43,7 @@ class FacebookController extends \yii\base\Controller{
             exit();
         }
 
-        if (! isset($accessToken)) 
-        {
+        if (! isset($accessToken)) {
             if ($this->helper->getError()) {
               header('HTTP/1.0 401 Unauthorized');
               echo "Error: " . $this->helper->getError() . "\n";
@@ -170,7 +55,7 @@ class FacebookController extends \yii\base\Controller{
               echo 'Bad request';
             }
             exit;
-        }
+          }
  
             var_dump($accessToken->getValue());
 
@@ -204,5 +89,4 @@ class FacebookController extends \yii\base\Controller{
 
 
     }
-    
 }
