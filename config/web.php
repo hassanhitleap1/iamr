9d519518d7b706ef55d6efb2ee8b8bfa9c036dfa
hasan <hassan.kiwan@gmail.com>
2018-02-27 09:25:11 +0200

    commit

--- config/web.php
+++ config/web.php
@@ -22,6 +22,9 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'JKNtnoyn?j29!$ndssPPJNM.==HIS',
         ],
+        'admin'=>[
+            
+        ],
          'i18n' => [
                 'translations' => [
                     'app*' => [
--- models/admin/LoginForm.php
+++ models/admin/LoginForm.php
@@ -50,7 +50,7 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
-           
+            
             return Yii::$app->admin->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
 
