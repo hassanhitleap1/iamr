395ae3329a360b61edc844d7e20f3a4714e0a476
hasan kiwan <email@example.com>
2018-03-03 09:41:47 -0800

    change name

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name'=>'i am riach ',
+    'name'=> 'i am rich',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
