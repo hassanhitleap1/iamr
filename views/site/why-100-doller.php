d459e8db5664c28366b02e5eaf04da0d1509da24
hasan kiwan <email@example.com>
2018-03-19 16:14:50 -0700

    convirt image from png to jpg

--- views/site/why-100-doller.php
+++ views/site/why-100-doller.php
@@ -14,7 +14,7 @@ $this->title = 'I AM RIACH';
             <div class="row">
                 <hr>
                 <div class="col-md-7  col-md-offset-2">
-                    <img src=<?= Yii::$app->request->baseUrl. '/image/page/1.jpg'?> alt="" class="img-blog img-rounded">
+                    <img src=<?= Yii::$app->request->baseUrl. '/image/page/1.png'?> alt="" class="img-blog img-rounded">
                 </div>
             </div>
             <div class="container">
@@ -31,90 +31,90 @@ $this->title = 'I AM RIACH';
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/1.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/1.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/3.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/3.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
         <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/6.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/6.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/8.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/8.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/13.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/13.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/14.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/14.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/15.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/15.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/16.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/16.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/17.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/17.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/18.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/18.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/12.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/12.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/26.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/26.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/13.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/13.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/14.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/14.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/19.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/19.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/20.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/20.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/21.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/21.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/22.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/22.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
                 <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/24.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/24.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/25.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/25.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
         <div class="row">
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/26.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/26.png' ?> alt="" class="img-blog img-thumbnail">
             </div> 
             <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/14.jpg' ?> alt="" class="img-blog img-thumbnail">
+            <img src=<?= Yii::$app->request->baseUrl . '/image/page/14.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
     </div>
