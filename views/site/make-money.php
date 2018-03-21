499cdaf4567d3e4aefe852c7165644da76cdb646
hasan kiwan <email@example.com>
2018-03-21 15:16:48 -0700

    add culcilation

--- views/site/make-money.php
+++ views/site/make-money.php
@@ -1,5 +1,6 @@
 <?php
-use yii\helpers\Url; 
+use yii\helpers\Url;
+use richardfan\widget\JSRegister;
 /* @var $this yii\web\View */
 $this->title =  Yii::t('app','make-money');
 ?>
@@ -28,5 +29,37 @@ $this->title =  Yii::t('app','make-money');
        
         </div>
     </div>
-
+    <div class="container calculator">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-2">
+                <div class="form-group">
+                <label for="ref">Number of Referral:</label>
+                <input type="number" class="form-control" id="ref" value="1">
+            </div>
+            </div>
+            <div class="col-md-6">
+                <div class="" style="font-size: 32px;">
+                    <span class="glyphicon glyphicon-usd" > </span> <strong id="amount">20 </strong>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<?php JSRegister::begin(); ?>
+<script>
+$(document).ready(function(){
+    $( "#ref" ).change(function() {
+    var number = $(this).val();
+    if( 0 >= number ){
+    $( "#amount" ).text( "error number must be real number" );
+    }else{
+    amount = number*20;
+    $( "#amount" ).text( amount );
+    }
+
+    
+    });
+});
+</script>
+<?php JSRegister::end(); ?>
