c59f76d3d7f4e423438c34800359352728b3d79b
hasan kiwan <email@example.com>
2018-09-14 05:38:54 -0700

    messages connect us

--- views/site/contact.php
+++ views/site/contact.php
@@ -21,23 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
             Thank you for contacting us. We will respond to you as soon as possible.
         </div>
 
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
-
     <?php else: ?>
-
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
 
         <div class="row">
             <div class="col-lg-5">
