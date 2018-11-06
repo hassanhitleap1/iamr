3c287f7ac611d3efecf428cb53b6b8d284a8fbb0
hasan kiwan <hassanhitleap1@gmail.com>
2018-11-06 15:48:31 -0800

    load more image

--- views/site/why-get-membership.php
+++ views/site/why-get-membership.php
@@ -1,5 +1,5 @@
 <?php
-use  yii\helpers\Url;
+use yii\helpers\Url;
 use richardfan\widget\JSRegister; 
 /* @var $this yii\web\View */
 $this->title = 'You are a rich';
@@ -10,12 +10,12 @@ $this->title = 'You are a rich';
         <div class="row" style="margin-top:65px">
 
             <div class="row">
-                <h1 class="title"><?= $model['title_'. Yii::$app->language]?></h1>
+                <h1 class="title"><?= $model['title_' . Yii::$app->language] ?></h1>
             </div>
             <div class="row">
                 <hr>
                 <div class="col-md-7  col-md-offset-2">
-                    <img src=<?= Yii::$app->request->baseUrl. '/image/page/1.png'?> alt="" class="img-blog img-rounded">
+                    <img src=<?= Yii::$app->request->baseUrl . '/image/page/1.png' ?> alt="" class="img-blog img-rounded">
                 </div>
             </div>
             <div class="container">
@@ -46,26 +46,11 @@ $this->title = 'You are a rich';
             <img src=<?= Yii::$app->request->baseUrl . '/image/page/8.png' ?> alt="" class="img-blog img-thumbnail">
             </div>   
         </div>
-        <div class="row">
-            <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/13.png' ?> alt="" class="img-blog img-thumbnail">
-            </div> 
-            <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/14.png' ?> alt="" class="img-blog img-thumbnail">
-            </div>   
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/15.png' ?> alt="" class="img-blog img-thumbnail">
-            </div> 
-            <div class="col-md-6">
-            <img src=<?= Yii::$app->request->baseUrl . '/image/page/16.png' ?> alt="" class="img-blog img-thumbnail">
-            </div>   
-        </div>
+    
 
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
-                <div  class="btn btn-primary btn-block" id="loadMore"><?=Yii::t('app', 'Load_more')?></div>
+                <div  class="btn btn-primary btn-block" id="loadMore"><?= Yii::t('app', 'Load_more') ?></div>
             </div>
         </div>
     </div>
@@ -77,7 +62,25 @@ $this->title = 'You are a rich';
 $(document).ready(function () {
     $("#loadMore").click(function (e) { 
         e.preventDefault();
+            
         var content='<div class="row">\n'+
+            '<div class="col-md-6">\n'+
+            '<img src=<?= Yii::$app->request->baseUrl . '/image/page/13.png' ?> alt="" class="img-blog img-thumbnail">\n'+
+            '</div> \n'+
+            '<div class="col-md-6">\n'+
+            '<img src=<?= Yii::$app->request->baseUrl . '/image/page/14.png' ?> alt="" class="img-blog img-thumbnail">\n'+
+            '</div>  \n'+ 
+        '</div>\n'+
+        
+        '<div class="row">\n'+
+            '<div class="col-md-6">\n'+
+            '<img src=<?= Yii::$app->request->baseUrl . '/image/page/15.png' ?> alt="" class="img-blog img-thumbnail">\n'+
+            '</div> \n'+
+            '<div class="col-md-6">\n'+
+            '<img src=<?= Yii::$app->request->baseUrl . '/image/page/16.png' ?> alt="" class="img-blog img-thumbnail">\n'+
+            '</div> \n'+  
+        '</div>\n'+
+        '<div class="row">\n'+
             '<div class="col-md-6">\n'+
             '<img src=<?= Yii::$app->request->baseUrl . '/image/page/17.png' ?> alt="" class="img-blog img-thumbnail">\n'+
             '</div>\n'+ 
