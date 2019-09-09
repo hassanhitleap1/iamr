18a71eee1ec0e11b893f29d562b6b0dad8b82720
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-09-09 14:08:33 -0700

    theme

--- web/theme/js/plugins/counterup.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-			"use strict";
-	            $('.counter').counterUp({
-	                delay: 10,
-	                time: 1000
-	            });
-        });
