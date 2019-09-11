93e639d57737f7179da8d404b027353aa26f97a6
hassan kiwan <hassanhitleap1@gmail.com>
2019-09-11 16:59:04 +0300

    param

--- config/params.php
+++ config/params.php
@@ -5,10 +5,9 @@
 $whitelist = array(
     '127.0.0.1',
     '::1',
-    'localhost:8080',
 );
 
-if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
     return [
         'adminEmail' => 'support@youarearich.org',
         'siteUrl'=> 'localhost:8080/web',
